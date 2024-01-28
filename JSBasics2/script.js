a9f4a03028cbf64d25935e39033706d237cd98db
bhavya singh <samgta9810@gmail.com>
2024-01-29 03:17:04 +0530

    JSbasics2 completed, for-in for-each loop pending garbage value pending

--- JSBasics2/script.js
+++ JSBasics2/script.js
@@ -25,41 +25,142 @@ console.log(0);
 
 //factory function with parameters
 
-function createRectangle(length, breadth) {
-  return (rectangle = {
-    length,
-    breadth,
+// function createRectangle(length, breadth) {
+//   return (rectangle = {
+//     length,
+//     breadth,
 
-    draw() {
-      console.log("draw");
-    },
-  });
-}
+//     draw() {
+//       console.log("draw");
+//     },
+//   });
+// }
 
-let obj1 = createRectangle(12, 23);
-let obj2 = createRectangle(10, 25);
+// let obj1 = createRectangle(12, 23);
+// let obj2 = createRectangle(10, 25);
 
-console.log(obj1.length);
-console.log(obj1.breadth);
-console.log(obj2.length);
-console.log(obj2.breadth);
+// console.log(obj1.length);
+// console.log(obj1.breadth);
+// console.log(obj2.length);
+// console.log(obj2.breadth);
 
 //factory function using parameters
 
-function person(name, age) {
-  let person = {};
+// function person(name, age) {
+//   let person = {};
 
-  person.name = name;
-  person.age = age;
+//   person.name = name;
+//   person.age = age;
 
-  person.greeting = function () {
-    return "good morning " + person.name;
+//   person.greeting = function () {
+//     return "good morning " + person.name;
+//   };
+//   return person;
+// }
+
+// let p1 = person("ravi", 22);
+// let p2 = person("kishan", 25);
+
+// console.log(p1.name);
+// console.log(p2.greeting());
+
+//constructor function ->
+
+function Rectangle() {
+  this.length = 1;
+  this.breadth = 2;
+  this.draw = function () {
+    console.log("drawing");
   };
-  return person;
 }
 
-let p1 = person("ravi", 22);
-let p2 = person("kishan", 25);
+let obj1 = new Rectangle();
 
-console.log(p1.name);
-console.log(p2.greeting());
+obj1.shape = "rectangle";
+delete obj1.shape;
+
+let Rectangle1 = new Function(
+  "length",
+  "breadth",
+  `this.length = length;
+this.breadth = breadth;
+this.draw = function () {
+  console.log("drawing");
+}`
+);
+
+//object creation using rectangle1
+
+let rectangle2 = new Rectangle1(2, 23);
+
+console.log(rectangle2);
+
+// functions are objects
+function icecream() {
+  const favIcecream = "chocolate";
+  console.log(`my favourite flavour is ${favIcecream}`);
+}
+
+icecream.flavour = ["chocolate", "vanilla", "mango"];
+
+icecream.showFlavour = function () {
+  return this.flavour;
+};
+
+console.log(icecream);
+
+//function are called first-class objects
+
+//they can be stored in variables
+const showIceCream = function () {};
+
+const arr = [22, "hello", showIceCream];
+
+const object1 = {
+  one: 22,
+  two: showIceCream,
+};
+
+console.log(object1);
+
+//they can be passed as parameters in another function
+
+function flavour() {
+  return "chocolate";
+}
+
+function findFlavour(func) {
+  console.log(`icecream flavour is ${func}`);
+}
+
+findFlavour(flavour());
+
+//types in js
+//primitive types
+//reference types or objects
+
+// let a = 10;
+// let b = a;
+
+// a++;
+// console.log(a);
+// console.log(b);
+
+// let c = { value: 10 };
+// let d = c;
+
+// c.value++;
+
+// console.log(c);
+// console.log(d);
+
+let a = 10;
+
+function increase(a) {
+  a++;
+  console.log(a);
+}
+
+increase(a);
+console.log(a);
+console.log();
