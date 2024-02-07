3e843092bc48b6115a489a181b805527d84d8a59
bhavya singh <samgta9810@gmail.com>
2024-02-08 01:26:07 +0530

    argument object ,default parameter, rest parameter topic s completed

--- JSBasics4/script.js
+++ JSBasics4/script.js
@@ -19,11 +19,23 @@ let static = stand;
 static();
 
 // dynamic nature of javascript on functions
+
 function sum(a, b) {
   for (let i = 0; i < arguments.length; i++) {
     console.log(` argument at ${i} = ${arguments[i]}`);
   }
 
+  //using for in loop which is used for objects
+  finalSum = 0;
+  for (let value in arguments) {
+    finalSum = finalSum + arguments[value];
+  }
+  total = 0;
+  for (let num of arguments) {
+    total = total + num;
+  }
+  console.log(finalSum);
+  console.log(total);
   console.log(`'a'=${a}`);
   console.log(`'b'=${b}`);
   console.log(arguments);
@@ -38,4 +50,21 @@ function sum(a, b) {
 // console.log(sum(1, 2));
 // console.log(sum(1));
 // console.log(sum());
-console.log(sum(11, 12, 23, 24, 35));
+console.log(sum(1, 2, 3, 5, 4));
+
+//rest operator (or rest parameter) in functions
+function summiung(a, b, c, d, e, ...vals) {
+  console.log(a, b, vals);
+}
+
+summiung(1, 23, 2, 3, 344, 32, 21, 67);
+
+//default parameter
+
+function interest(p, r = 7, y = 10) {
+  return (p * r * y) / 100;
+}
+// console.log(interest(1000, 8));
+console.log(interest(1000, undefined, 8));
+
+//getter ,setters
