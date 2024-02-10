c7be0bb1fe9333b6de35525b370f3b471d414440
bhavya singh <samgta9810@gmail.com>
2024-02-10 23:11:51 +0530

    jsbasics 4 completed

--- JSBasics4/script.js
+++ JSBasics4/script.js
@@ -68,21 +68,174 @@ function interest(p, r = 7, y = 10) {
 console.log(interest(1000, undefined, 8));
 
 //getter ,setters
+// let person = {
+//   fName: "Bhavya",
+//   lName: "Singh",
+//   get fullName() {
+//     return `${person.fName} ${person.lName}`;
+//   },
+
+//   set fullName(Name) {
+//     let divide = Name.split(" ");
+//     this.fName = divide[0];
+//     this.lName = divide[1];
+//   },
+// };
+
+// console.log(person);
+
+// person.fullName = "Rajkumar Rao ";
+// console.log(person.fullName);
+
 let person = {
+  firstName: "JOhn",
+  lastName: "Doe",
+  lang: "en",
+
+  get language() {
+    return this.lang;
+  },
+
+  set language(lang) {
+    this.lang = lang;
+  },
+};
+
+person.language = "HIN";
+console.log(person.language);
+
+//javascript function or getter
+
+let person1 = {
+  firstName: "JOhn",
+  lastName: "Doe",
+  fullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+console.log(person1.fullName());
+
+let person2 = {
+  firstName: "john",
+  lastName: "cena",
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+console.log(person2.fullName.toUpperCase());
+
+let person3 = {
+  firstName: "john",
+  lastName: "cena",
+  language: "",
+  get lang() {
+    return this.language;
+  },
+
+  set lang(language) {
+    this.language = language.toUpperCase();
+  },
+};
+person3.lang = "hindi";
+console.log(person3.lang);
+
+//try and catch
+
+let person4 = {
   fName: "Bhavya",
   lName: "Singh",
   get fullName() {
-    return `${person.fName} ${person.lName}`;
+    return `${person4.fName} ${person4.lName}`;
   },
 
   set fullName(Name) {
+    console.log(typeof Name);
+    if (typeof Name !== String) {
+      throw new Error("It uses string only");
+    }
     let divide = Name.split(" ");
     this.fName = divide[0];
     this.lName = divide[1];
   },
 };
 
-console.log(person);
+// console.log(person4);
+// try {
+//   person4.fullName = "Shubham singh";
+// } catch (e) {
+//   alert(e);
+// }
+// console.log(person4.fullName);
 
-person.fullName = "Rajkumar Rao ";
-console.log(person.fullName);
+//array of objects ,filter method and console.table
+const players = [
+  { name: "Ronaldo", age: 37, team: ["Madrid", "United"] },
+  {
+    name: "Salah",
+    age: 30,
+    team: ["Roma", "Liverpool"],
+  },
+  {
+    name: "Haaland",
+    age: 22,
+    team: ["Dortmund", "City"],
+  },
+  {
+    name: "Bruyne",
+    age: 31,
+    team: ["Wolfsburg", "City"],
+  },
+];
+
+console.table(
+  //   players.filter((player) => player.age < 30),
+  players.filter((p) => p.team.includes("City"))
+);
+
+//scopes
+
+if (true) {
+  var a = 5;
+}
+
+console.log(a);
+
+for (var i = 0; i < 5; i++) {}
+console.log(i);
+
+function a() {
+  const ab = 5;
+}
+
+function b() {
+  const ab = 5;
+}
+
+const d = 15;
+
+function c() {
+  const d = 18;
+  console.log(d);
+}
+
+c();
+console.log(d);
+
+//reducing an array
+
+let arr = [23, 24, 55, 16];
+let ttl = 0;
+for (let val of arr) {
+  ttl += val;
+}
+console.log(ttl);
+
+let arr2 = [2, 4, 6, 8];
+
+let final = 0;
+arr2.forEach((val) => (final = final + val));
+
+console.log(final);
